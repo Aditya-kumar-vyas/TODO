31f30b8be8b39761a0477f494ff0bb76c78911fb
Aditya-kumar-vyas <kumarvyasaditya@gmail.com>
2023-10-16 14:30:53 +0530

    This is first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: const Text('TO DO'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView(
+
+      ),
+
+    );
+  }
+}
